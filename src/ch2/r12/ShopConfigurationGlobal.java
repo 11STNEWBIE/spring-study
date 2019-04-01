3d2547671800af0ba0ae21e99a75d6f2f0e0b681
김진우(Jinwoo Kim/Finding개발팀/11ST) <jinwoojimmy@sk.com>
2019-04-01 11:00:18 +0900

    ADD recipe 2.12

---
+++ src/ch2/r12/ShopConfigurationGlobal.java
@@ -0,0 +1,21 @@
+package ch2.r12;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("global")
+@ComponentScan("ch2.r12")
+public class ShopConfigurationGlobal {
+
+    @Bean(initMethod = "openFile", destroyMethod = "closeFile")
+    public Cashier cashier() {
+        final String path = System.getProperty("java.io.tmpdir") + "cashier";
+        Cashier c1 = new Cashier();
+        //c1.setFileName("checkout");       <- beanName 가져와서 설정하는 경우 불필요~
+        c1.setPath(path);
+        return c1;
+    }
+}

f708b75217a910df0abb15d562893e15af18ffdb
Jiwon Kang <doradora.by.doris@gmail.com>
2019-05-14 19:50:01 +0900

    Add 3-9~3-11

---
+++ chapter03/3-9~3-11/java/com/apress/springrecipes/court/baseball/Game.java
@@ -0,0 +1,13 @@
+package com.apress.springrecipes.court.baseball;
+
+public class Game {
+
+    public Game(int ... nums) {
+
+    }
+
+    public void guess(int ... g) {
+//        throw new IllegalArgumentException();
+    }
+
+}
---
+++ chapter03/3-9~3-11/java/com/apress/springrecipes/court/baseball/Score.java
@@ -0,0 +1,21 @@
+package com.apress.springrecipes.court.baseball;
+
+public class Score {
+
+    private int balls;
+    private int strikes;
+
+    public void Score(int balls, int strikes) {
+        this.balls = balls;
+        this.strikes = strikes;
+    }
+
+    public void balls() {
+
+    }
+
+    public void strikes() {
+
+    }
+
+}
